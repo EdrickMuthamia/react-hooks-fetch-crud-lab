a12067c3266f3c5e0eb1e6ce7138e09462358db4
EdrickMuthamia <edrickmuthamia05@gmail.com>
2025-07-26 19:03:09 +0300

    add updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((data) => setQuestions(data));
+  }, []);
+
+  function handleAddQuestion(newQuestion) {
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newQuestion),
+    })
+      .then((r) => r.json())
+      .then((data) => setQuestions([...questions, data]));
+  }
+
+  function handleDeleteQuestion(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      setQuestions(questions.filter((question) => question.id !== id));
+    });
+  }
+
+  function handleUpdateQuestion(id, updatedData) {
+    const updatedQuestions = questions.map((q) => {
+      if (q.id === id) {
+        return { ...q, ...updatedData };
+      }
+      return q;
+    });
+    setQuestions(updatedQuestions);
+    
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedData),
+    });
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm onAddQuestion={handleAddQuestion} />
+      ) : (
+        <QuestionList 
+          questions={questions} 
+          onDeleteQuestion={handleDeleteQuestion}
+          onUpdateQuestion={handleUpdateQuestion}
+        />
+      )}
     </main>
   );
 }
